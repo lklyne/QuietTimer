1b5728b397f5966a9899787dae1bcdd0a5374c25
lklyne <lyle.klyne@gmail.com>
2025-06-13 20:33:14 -0700

    Initial Commit

---
+++ QuietTimer/QuietTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuietTimerApp.swift
+//  QuietTimer
+//
+//  Created by Lyle Klyne on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct QuietTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuietTimerTests/QuietTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuietTimerTests.swift
+//  QuietTimerTests
+//
+//  Created by Lyle Klyne on 6/13/25.
+//
+
+import Testing
+@testable import QuietTimer
+
+struct QuietTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
